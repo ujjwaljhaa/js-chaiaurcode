9bbd6180967a5fc10ca8ea3ade72e493e292c416
Ujjwal Jha <115773921+ujjwaljhaa@users.noreply.github.com>
2025-12-29 06:59:32 +0000

    Learnt variable and datatypes in js

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 12234;
+let accountEmail = "ujjwal@google.com";
+var accountPassword = "13625";
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "uj@uj.com";
+accountPassword = "89898";
+accountCity = "mumbai";
+
+console.log(accountId);
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
